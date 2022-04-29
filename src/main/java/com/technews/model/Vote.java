37624ab96c5c5b0c9bc515a6b536fd488d9ec4c1
Nicholas Nolen <nicnolen@ymail.com>
2022-04-29 14:48:14 -0500

    added class-level annotations to the Vote model

--- src/main/java/com/technews/model/Vote.java
+++ src/main/java/com/technews/model/Vote.java
@@ -1,7 +1,14 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "vote")
 public class Vote implements Serializable {
     private Integer id;
     private Integer userId;
