c9be9d30e9a6964c221ba4dc8159aad9476e74b0
Nicholas Nolen <nicnolen@ymail.com>
2022-04-29 12:14:43 -0500

    created the instance variable-level annotations for the user class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,8 +1,8 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 
 // Properties to be ignored when serializing this object to JSON
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -11,10 +11,18 @@ import javax.persistence.Table;
 // Name of the table the class maps to
 @Table(name = "user")
 public class User {
+    // id will be used as the unique identifier
+    @Id
+    // id will be a generated value and should be generated automatically
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String username;
+    // value must be unique
+    @Column(unique = true)
     private String email;
     private String password;
+    // data will not be persisted in the database
+    @Transient
     boolean loggedIn;
 
     // lists are collections of objects of the same type
