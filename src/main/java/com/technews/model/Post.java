3a3562600417ad0bea0cbe6f7c919e3bcecc3f60
Nicholas Nolen <nicnolen@ymail.com>
2022-04-29 14:21:37 -0500

    added class-level annotations

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,7 +1,14 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "post")
 public class Post implements Serializable {
     private Integer id;
     private String title;
