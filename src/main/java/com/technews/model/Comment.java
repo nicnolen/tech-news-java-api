144939f5ecfaa03397ac1d31b7200ea5e67520d4
Nicholas Nolen <nicnolen@ymail.com>
2022-04-29 14:37:28 -0500

    added class-level annotations to the Comment model

--- src/main/java/com/technews/model/Comment.java
+++ src/main/java/com/technews/model/Comment.java
@@ -1,7 +1,14 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name="comment")
 public class Comment implements Serializable {
     private Integer id;
     private String commentText;
