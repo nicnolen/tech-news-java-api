d77b05c0ea3c9ece433a82402d896d0e5ea31e29
Nicholas Nolen <nicnolen@ymail.com>
2022-04-29 10:54:13 -0500

    generated constructor in Demo.java file

---
+++ src/main/java/com/technews/testModel/Demo.java
@@ -0,0 +1,12 @@
+package com.technews.testModel;
+
+public class Demo {
+    private String name;
+
+    public Demo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private int age;
+}
