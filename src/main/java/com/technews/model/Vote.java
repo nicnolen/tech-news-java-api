d50b0fd503a6168d346e3e26e062ea1b3e51de6b
Nicholas Nolen <nicnolen@ymail.com>
2022-04-29 14:46:20 -0500

    created the vote model and added instance variables

---
+++ src/main/java/com/technews/model/Vote.java
@@ -0,0 +1,9 @@
+package com.technews.model;
+
+import java.io.Serializable;
+
+public class Vote implements Serializable {
+    private Integer id;
+    private Integer userId;
+    private Integer postId;
+}
