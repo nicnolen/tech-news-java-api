20d7d04dea134a8addd597a8d3e103fd026e884a
Nicholas Nolen <nicnolen@ymail.com>
2022-05-02 15:18:26 -0500

    added a POST route to edit comments

--- src/main/java/com/technews/controller/TechNewsController.java
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -132,4 +132,19 @@ public class TechNewsController {
             }
         }
     }
+    @PostMapping("/comments/edit")
+    public String createCommentEditPage(@ModelAttribute Comment comment, HttpServletRequest request) {
+        if (comment.getCommentText().equals("") || comment.getCommentText().equals(null)) {
+            return "redirect:/editPostEmptyComment/" + comment.getPostId();
+        } else {
+            if (request.getSession(false) != null) {
+                User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+                comment.setUserId(sessionUser.getId());
+                commentRepository.save(comment);
+                return "redirect:/dashboard/edit/" + comment.getPostId();
+            } else {
+                return "redirect:/login";
+            }
+        }
+    }
 }
