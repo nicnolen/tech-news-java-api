9264c4ee8fe05db687c8bd9b23a38abf38d500fa
Nicholas Nolen <nicnolen@ymail.com>
2022-05-02 14:23:48 -0500

    added a login route in the HomePageController.java file

--- src/main/java/com/technews/controller/HomePageController.java
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -1,11 +1,16 @@
 package com.technews.controller;
 
+import com.technews.model.User;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 // indicate that these controllers will control flow for the front-end user experience
 @Controller
@@ -21,4 +26,15 @@ public class HomePageController {
 
     @Autowired
     CommentRepository commentRepository;
+
+    @GetMapping("/login")
+    // Model model is remapping Model to the model variable
+    public String login(Model model, HttpServletRequest request) {
+        if (request.getSession(false) != null) {
+            return "redirect:/";
+        }
+        // addAttribute() sends information to Thymeleaf
+        model.addAttribute("user", new User());
+        return "login";
+    }
 }
