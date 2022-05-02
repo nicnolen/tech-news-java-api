64d909361ede6354383ce0a30f8d5a033c3c6b80
Nicholas Nolen <nicnolen@ymail.com>
2022-05-02 10:32:25 -0500

    added get method to get all users and added annotations to the UserController file

---
+++ src/main/java/com/technews/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.technews.controller;
+
+import com.technews.model.Post;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// Allows Spring to process JSON/XML responses and send JSON/XML objects via the API
+@RestController
+public class UserController {
+    // tells Spring to only instantiate each object as needed by the program
+    @Autowired
+    UserRepository repository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    // combines the route ("/api/users") and the type of HTTP method used (GET) to provide the method with a unique endpoint
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        List<User> userList = repository.findAll();
+        for (User u : userList) {
+            List<Post> postList = u.getPosts();
+            for (Post p : postList) {
+                p.setVoteCount(voteRepository.countVotesByPostId((p.getId())));
+            }
+        }
+        return userList;
+    }
+}
