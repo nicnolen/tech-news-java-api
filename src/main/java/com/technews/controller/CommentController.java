445d1841c8f88c2c335fe7cc7006c44b6fe65d69
Nicholas Nolen <nicnolen@ymail.com>
2022-05-02 11:52:14 -0500

    added GET route to get comments by id in CommentController file

--- src/main/java/com/technews/controller/CommentController.java
+++ src/main/java/com/technews/controller/CommentController.java
@@ -1,17 +1,28 @@
 package com.technews.controller;
 
+import com.technews.model.Comment;
 import com.technews.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CommentController {
     @Autowired
     CommentRepository repository;
 
+    // get all comments
     @GetMapping("/api/comments")
     public List<Comment> getAllComments() {
         return repository.findAll();
+    }
+
+    // get comments by id
+    @GetMapping("api/comments/{id}")
+    public Comment getComment(@PathVariable int id) {
+        return repository.getById(id);
     }
 }
