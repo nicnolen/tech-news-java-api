9427c4a8320d353e4774992c197fd2e335cced1c
Nicholas Nolen <nicnolen@ymail.com>
2022-05-02 14:20:06 -0500

    created the homepage controller

---
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -0,0 +1,24 @@
+package com.technews.controller;
+
+import com.technews.repository.CommentRepository;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+// indicate that these controllers will control flow for the front-end user experience
+@Controller
+public class HomePageController {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+}
