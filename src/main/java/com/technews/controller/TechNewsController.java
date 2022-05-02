bb4f2414618190386a35a1e88f9ccd81ce1807b2
Nicholas Nolen <nicnolen@ymail.com>
2022-05-02 15:16:49 -0500

    added PUT route to update a post

--- src/main/java/com/technews/controller/TechNewsController.java
+++ src/main/java/com/technews/controller/TechNewsController.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -100,6 +101,19 @@ public class TechNewsController {
             User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
             post.setUserId(sessionUser.getId());
             postRepository.save(post);
+            return "redirect:/dashboard";
+        }
+    }
+
+    @PostMapping("/posts/{id}")
+    public String updatePostDashboardPage(@PathVariable int id, @ModelAttribute Post post, Model model, HttpServletRequest request) {
+        if (request.getSession(false) == null) {
+            model.addAttribute("user", new User());
+            return "redirect/dashboard";
+        } else {
+            Post tempPost = postRepository.getById(id);
+            tempPost.setTitle(post.getTitle());
+            postRepository.save(tempPost);
             return "redirect:/dashboard";
         }
     }
